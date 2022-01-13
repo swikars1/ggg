fb65ef5dc3ba115e693b6c786f8d63cd6771d1c6
swikars1 <swikarsharma@gmail.com>
2022-01-13 14:18:02 +0545

    gen for all

--- gin-gorm-gen.sh
+++ gin-gorm-gen.sh
@@ -15,10 +15,25 @@ read plural_resource
 lc_resource=`echo $uc_resource | awk '{$1=tolower(substr($1,0,1))substr($1,2)}1'`
 plc_resource=`echo $plural_resource | awk '{$1=tolower(substr($1,0,1))substr($1,2)}1'`
 
+
+# routes gen
 sed "s/{{ucresource}}/$uc_resource/g" ./route-template.txt > "./${resource_table}.go"
-sed -i "" "s/{{plcresource}}/$plc_resource/g" "./${resource_table}.go"
 sed -i "" "s/{{lcresource}}/$lc_resource/g" "./${resource_table}.go"
-sed -i "" "s/{{resourcetable}}/$resource_table/g" "./${resource_table}.go"
 sed -i "" "s/{{projectname}}/$project_name/g" "./${resource_table}.go"
 
+# controller gen
+sed "s/{{ucresource}}/$uc_resource/g" ./controller-template.txt > "./${resource_table}.go"
+sed -i "" "s/{{plcresource}}/$plc_resource/g" "./${resource_table}.go"
+sed -i "" "s/{{lcresource}}/$lc_resource/g" "./${resource_table}.go"
+sed -i "" "s/{{projectname}}/$project_name/g" "./${resource_table}.go"
 
+# service gen
+sed "s/{{ucresource}}/$uc_resource/g" ./service-template.txt > "./${resource_table}.go"
+sed -i "" "s/{{lcresource}}/$lc_resource/g" "./${resource_table}.go"
+sed -i "" "s/{{projectname}}/$project_name/g" "./${resource_table}.go"
+
+# repo gen
+sed "s/{{ucresource}}/$uc_resource/g" ./repo-template.txt > "./${resource_table}.go"
+sed -i "" "s/{{plcresource}}/$plc_resource/g" "./${resource_table}.go"
+sed -i "" "s/{{resourcetable}}/$resource_table/g" "./${resource_table}.go"
+sed -i "" "s/{{projectname}}/$project_name/g" "./${resource_table}.go"
